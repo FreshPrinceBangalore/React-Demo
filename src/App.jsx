0be5af84e11361762e7ff3eb2ffbf8f78bc6f874
“Albis” <albisabeldas99@gmail.com>
2024-03-27 11:46:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx';
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+
+    </>
+  );
+}
+
+export default App
---
+++ src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+
+    let food1 = "Biryani";
+    let food2 = "Kabab";
+
+    return(
+        <ul>
+            <li>Kushka</li>
+            <li>{food1.toUpperCase()}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+
+}
+
+export default Food;
